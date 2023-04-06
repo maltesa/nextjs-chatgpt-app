c0bb5cb2e394d1ad880bb8dd1a34f2d9fde26a50
Malte Hecht <malte@siggi.cloud>
2023-04-06 14:49:27 +0000

    fix code highlighting

--- components/Chat/Messages/parseMessage.ts
+++ components/Chat/Messages/parseMessage.ts
@@ -1,4 +1,12 @@
 import Prism from 'prismjs'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-java'
+import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-json'
+import 'prismjs/components/prism-markdown'
+import 'prismjs/components/prism-python'
+import 'prismjs/components/prism-typescript'
+import 'prismjs/themes/prism-okaidia.css'
 
 import { MessageBlock } from './types'
 
@@ -28,7 +36,7 @@ export function parseMessage(text: string): MessageBlock[] {
     const highlightLanguage = codeLanguage || 'typescript'
     const highlightedCode = Prism.highlight(
       code,
-      Prism.languages[highlightLanguage]!,
+      Prism.languages[highlightLanguage] || Prism.languages['typescript']!,
       highlightLanguage
     )
 
